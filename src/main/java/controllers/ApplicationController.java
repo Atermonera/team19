5909f4547857500162dcf0e195eca205858e3ccd
Atermonera <atermonera@gmail.com>
2017-11-07 13:47:26 -0800

    Implement Stack, add framework for others

--- src/main/java/controllers/ApplicationController.java
+++ src/main/java/controllers/ApplicationController.java
@@ -34,10 +34,6 @@ public class ApplicationController {
     
     public Result gameGet(){
         Game g = new Game();
-        g.buildDeck();
-        g.shuffle();
-        g.dealFour();
-
         return Results.json().render(g);
     }
 
---
+++ src/main/java/models/Columns.java
@@ -0,0 +1,24 @@
+package models;
+
+public class Columns{
+	
+	private Stack stacks[];
+	
+	public Columns(){
+		// Constructor
+	}
+	
+	public void deal(Card a, Card b, Card c, Card d){
+		// Move a, b, c, and d onto the columns.
+		// Argument names subject to change
+	}
+	
+	public void move(int col1, int col2){
+		// Try to move the top card from col1 to col2
+	}
+	
+	public void remove(int col){
+		// Try to remove the top card from col1	
+	}
+	
+}
