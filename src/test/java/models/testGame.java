daf79874e9e25e45416aca2a9d4b268a13a5cfc7
William Wodrich <wodrichw@oregonstate.edu>
2017-12-01 19:24:46 -0800

    Junit set2 (#72)
    JUnit Set 2

--- src/test/java/models/testGame.java
+++ src/test/java/models/testGame.java
@@ -3,6 +3,7 @@ package models;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.zip.CRC32;
 
 import static org.junit.Assert.*;
 
@@ -53,8 +54,21 @@ public class testGame {
         g.customDeal(new Card(2, Suit.StdC), new Card(3, Suit.StdC), new Card(4, Suit.StdC), new Card(5, Suit.StdC));
         g.remove(2);
         assertEquals(0,g.cols[2].getSize());
+
     }
 
 
-
+    @Test
+    public void testGameDealLowDeck(){
+        Game g = new Game(1);
+        while (g.deck.getSize() > 4){//deal cards until there are 4 or less, in the case of the Spanish deck, 2 left
+            g.dealFour();
+        }
+       assertEquals(g.deck.getSize(), 2);
+        Card card0 = g.deck.stack.get(0);
+        Card card1 = g.deck.stack.get(1);
+        g.dealFour();
+        assertEquals(card0, g.cols[2].peek());
+        assertEquals(card1, g.cols[1].peek());
+    }
 }
--- src/test/java/models/testStack.java
+++ src/test/java/models/testStack.java
@@ -13,4 +13,14 @@ public class testStack {
         return;
     }
 
+   @Test
+   public void testPop(){
+        Stack s = new Stack();
+        Card toPush0 = new Card(5, Suit.StdH);
+        Card toPush1 = new Card(2, Suit.StdH);
+        s.push(toPush0);
+        s.push(toPush1);
+        assertEquals(toPush1, s.pop());
+        assertEquals(toPush0, s.stack.get(0));
+   }
 }
